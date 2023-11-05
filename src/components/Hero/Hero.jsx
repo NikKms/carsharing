dbf2d1650fdab6967c146bcb9e1772b2a493b19c
NikKms <mykola.kamyshnikov@gmail.com>
2023-11-05 23:45:30 +0200

    delete rus flag from bg =)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,13 +2,13 @@ import { NavLink } from 'react-router-dom';
 import CustomSlider from '../CustomSlider/CustomSlider';
 import { Box, Button, Container, Heading } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import bgHero1 from '../../assets/img/1.jpg';
-import bgHero2 from '../../assets/img/2.jpg';
-import bgHero3 from '../../assets/img/3.jpg';
-import bgHero6 from '../../assets/img/6.jpg';
-import bgHero7 from '../../assets/img/7.jpg';
+import bgH1 from '../../assets/img/1.jpg';
+import bgH2 from '../../assets/img/2.jpg';
+import bgH3 from '../../assets/img/3.jpg';
+import bgH6 from '../../assets/img/6.jpg';
+import bgH5 from '../../assets/img/5.jpg';
 
-const data = [bgHero1, bgHero2, bgHero3, bgHero6, bgHero7];
+const data = [bgH1, bgH2, bgH3, bgH6, bgH5];
 
 const Hero = () => {
 	const { t } = useTranslation();
